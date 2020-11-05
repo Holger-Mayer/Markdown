634c0466ca2191a9615c76431cd115eea50a3160
Holger Mayer <holger.mayer@ewetel.net>
2020-11-05 10:12:36 +0100

    Neues Logo

--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -9,6 +9,9 @@ import XCTest
 
 class Tests_iOS: XCTestCase {
 
+    var app : XCUIApplication!
+    
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -22,13 +25,58 @@ class Tests_iOS: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+ 
+    func startApp() {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        
+        app = XCUIApplication()
         app.launch()
-
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCUIDevice.shared.orientation = .landscapeLeft
+        
+        app/*@START_MENU_TOKEN@*/.collectionViews["Browse View"].cells["DOC.sidebar.item.On My iPad"]/*[[".otherElements.matching(identifier: \"Browse View\").collectionViews[\"Browse View\"]",".cells[\"On My iPad\"]",".cells[\"DOC.sidebar.item.On My iPad\"]",".collectionViews[\"Browse View\"]"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.otherElements.containing(.image, identifier:"ipad").element.tap()
+        app/*@START_MENU_TOKEN@*/.collectionViews["File View"]/*[[".otherElements.matching(identifier: \"Browse View\")",".otherElements[\"DOC.browsingRoot Source: com.apple.FileProvider.LocalStorage, Title: File Provider Storage\"].collectionViews[\"File View\"]",".collectionViews[\"File View\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.cells["Create Document"].children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).tap()
+
+    }
+    
+    func endDocumentEdit(){
+        XCUIApplication().navigationBars["Markdown"].buttons["Back"].tap()
+    }
+    
+    func testExample() throws {
+         
+        startApp()
+        
+        let textView = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element
+        textView.tap()
+        
+        let markdownNavigationBar = app.navigationBars["Markdown"]
+        
+        markdownNavigationBar.buttons["Chapter"].tap()
+        textView.tap()
+        textView.tap()
+        markdownNavigationBar.buttons["Section"].tap()
+        textView.tap()
+        textView.tap()
+
+        app.keys["H"].tap()
+        app.keys["u"].tap()
+        app.keys["g"].tap()
+        app.keys["o"].tap()
+
+        markdownNavigationBar.otherElements.containing(.button, identifier:"Chapter").element.tap()
+        markdownNavigationBar.buttons["italic"].tap()
+          
+         
+        app.keys["H"].tap()
+        app.keys["u"].tap()
+        app.keys["g"].tap()
+        app.keys["o"].tap()
+
+        endDocumentEdit()
     }
 
     func testLaunchPerformance() throws {
@@ -40,3 +88,5 @@ class Tests_iOS: XCTestCase {
         }
     }
 }
+
+
